08e08a4360e89449038a5e0b94c260ffecfd4fb3
hannah68 <68994580+hannah68@users.noreply.github.com>
2021-02-25 17:22:57 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('#button');
+
+button.addEventListener('click',function(){
+	const iconContainer = document.querySelector('#icon-container');
+
+	iconContainer.classList.toggle('show');
+})
+
+
+
+
+
+
+
+  
